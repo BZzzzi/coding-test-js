d8aa4f06e75edc51a528700528cb7bad294c3e00
BZzzzi <khj23465@naver.com>
2025-09-24 00:29:59 +0900

    백준 2577 완료

--- BackJoon_basic/BackJoon_2577.js
+++ BackJoon_basic/BackJoon_2577.js
@@ -1,1 +1,19 @@
 let fs = require("fs");
+let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
+
+let result = 1;
+let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+for (let i = 0; i < input.length; i++) {
+  result *= input[i];
+}
+
+result = String(result);
+
+for (let charNum of result) {
+  arr[charNum]++;
+}
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
